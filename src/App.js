af622e2cb567cb65bff3780921fa919c78c2b710
iTsygancov <i.tsygancov@yandex.ru>
2021-10-25 01:22:47 +0300

    Creating todo object

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
+import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="todo">
-      <Form />
+      <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} />
       <TodoList />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,33 @@
-function Form() {
+function Form({ inputText, setInputText, todos, setTodos }) {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+  const submitTodosHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        title: inputText,
+        completed: false,
+        id: Math.floor(Math.random * 100000),
+      },
+    ]);
+  };
+
   return (
     <form className="todo__form">
-      <input className="todo__form-input" type="text" />
-      <button className="button todo__form--button">Create</button>
+      <input
+        className="todo__form-input"
+        type="text"
+        value={inputText}
+        onChange={inputTextHandler}
+      />
+      <button
+        className="button todo__form--button"
+        onClick={submitTodosHandler}
+      >
+        Create
+      </button>
     </form>
   );
 }
